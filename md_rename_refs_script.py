d647f4b5abbb70b404490e74f033cb067162dfa4
Jared <jaredbbowden@gmail.com>
2021-07-20 10:54:15 -0500

    Script control flow

--- md_rename_refs_script.py
+++ md_rename_refs_script.py
@@ -10,6 +10,9 @@ def rename_file_references(source_file, new_path):
         new_path (str): the path to the new file to be used as a reference. 
         don't include the final slash.
     """    
+    
+    # TODO I don't see how this can just be a file name and not a full path...
+    
     with open(source_file, 'r') as file:
         filedata = file.read()
 
@@ -41,6 +44,9 @@ def rename_all_file_references(base_directory, new_path):
         to replace
         new_path (string): string to the preferred image directory
     """
+    
+    # TODO I think we're going to need some more path normalization in here
+    
     file_paths = find_notebooks_and_markdown_files(base_directory)
     print("Found: ", len(file_paths))
 
@@ -49,21 +55,26 @@ def rename_all_file_references(base_directory, new_path):
         rename_file_references(os.path.basename(one_file), new_path)
 
 
-# TODO starting to think that I might do better to do this with some input 
-# prompts input("File name: ")
+# Let's do this right.
+if name == "__main__":
 
+    check_mode = input("Run on single file (y/n)?: ")
 
-check_mode = input("Run on single file (y/n)?: ")
+    if check_mode = "y":
+        print("Single file mode")
 
-if check_mode = "y":
-    print("Single file mode")
+        base_directory = input("Enter path to base_directory: ")
+        new_path = input("Enter _new_ relative path to images: ")
 
+        rename_file_references(source_file, new_path)
 
-if check_mode == "n":
-    print("Directory mode mode")
+    elif check_mode == "n":
+        print("Directory mode mode")
 
-    source_file = input("Source file: ")
-    new_path = input("Source file: ")
+        base_directory = input("Enter path to base_directory: ")
+        new_path = input("Enter _new_ relative path to images: ")
 
-else:
-    print("Please enter y or n")
+        rename_all_file_references(base_directory, new_path)
+
+    else:
+        print("Please enter y or n")
