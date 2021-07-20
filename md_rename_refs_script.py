16811700d936f5f58430d813a7089d4b8bbd7043
Jared <jaredbbowden@gmail.com>
2021-07-20 12:17:27 -0500

    formatting

--- md_rename_refs_script.py
+++ md_rename_refs_script.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python
 
 
+from markdown_images import find_notebooks_and_markdown_files
+
+
 def rename_file_references(source_file, new_path):
     """Open a file, find all markdown references, replace with the new path. 
-    
+
     Args:
         source_file (str): the _name_ (not the path) of the file that you're
          modifying
         new_path (str): the path to the new file to be used as a reference. 
         don't include the final slash.
-    """    
-    
+    """
+
     # TODO I don't see how this can just be a file name and not a full path...
-    
+
     with open(source_file, 'r') as file:
         filedata = file.read()
 
-    # From use, I've found that I can be a little inconsistent with how 
+    # From use, I've found that I can be a little inconsistent with how
     # I enter the extra string. It needs to _not_ have the final slash
     if new_path[-1] != "/":
         new_path = new_path + "/"
@@ -30,7 +33,7 @@ def rename_file_references(source_file, new_path):
 
 
 # TODO let's make a function to do this over all images in a directory,
-# then move this to the script file. 
+# then move this to the script file.
 
 # I think that move markdown still has a place, particularly for when files
 # need to move to a totally new path (ie we want to move the file AND images)
@@ -44,9 +47,9 @@ def rename_all_file_references(base_directory, new_path):
         to replace
         new_path (string): string to the preferred image directory
     """
-    
+
     # TODO I think we're going to need some more path normalization in here
-    
+
     file_paths = find_notebooks_and_markdown_files(base_directory)
     print("Found: ", len(file_paths))
 
@@ -56,11 +59,11 @@ def rename_all_file_references(base_directory, new_path):
 
 
 # Let's do this right.
-if name == "__main__":
+if __name__ == "__main__":
 
     check_mode = input("Run on single file (y/n)?: ")
 
-    if check_mode = "y":
+    if check_mode == "y":
         print("Single file mode")
 
         base_directory = input("Enter path to base_directory: ")
@@ -78,3 +81,12 @@ if name == "__main__":
 
     else:
         print("Please enter y or n")
+
+
+# TODO I would prefer to _import_ these functions from the helper file.
+# Note that the same goes for the md_image_script file, too
+
+# I think that this will also allow us to avoid all these imports...
+
+# Also, the more I look around here, the more I see that there are more
+# _internal_ functions that I have to pull in, too.
