c4de9ad67b12a462456fd670d81b08ba6aaac86a
Jared <jaredbbowden@gmail.com>
2021-07-21 19:50:36 -0500

    New move image script

--- markdown_images.py
+++ markdown_images.py
@@ -3,91 +3,98 @@ import platform
 import glob
 from datetime import datetime
 import re
-import pdb
+from shutil import copyfile
 
 
 # TODO Well... [gestures broadly] there's a lot. Base functionality is there
 # remaining updates are tagged. I should really have a develop branch for this
 # project, but that's not how we're doing things.
 
+def system_image_path():
+    """Find the path to the system image directory
+    
+    Returns: string, path to the system image directory
+    """
+    if platform.system() == "Darwin":
+        image_path = "~/Desktop/"
+        system_OS = "Mac"
+    elif platform.system() == "Linux":
+        image_path = "~/Pictures/"
+        system_OS = "Linux"
 
-def markdown_image(n_files=1):
+    return image_path, system_OS
+
+
+def string_to_clipboard(the_string, system_OS):
+    """send a string to the clipboard
+
+    Args:
+        the_string (string): the sting you want to send to the clipboard
+        system_OS (string): string describing the system OS you're using
+    """    
+    if system_OS == "Mac":
+        os.system("echo " + "'" + the_string + "'" + " | pbcopy")
+        
+    elif system_OS == 'Linux':
+        os.system("echo " + "'" +
+                  the_string + "'" + " | xclip -sel clip")
+
+
+def markdown_image(file_destination_path):
     """    
     Pull the n most recent screenshots, re-title, move a destination of your choosing,
     and copy a formatted markdown snippet to the system clipboard.
 
-    n_files: the number of files you want to move
+    Args:
+        file_destination_path (string): path to the file where the image will 
+        go. Note that this does _not_ include the path to the `images` 
+        directory.
 
     Dependencies
     * xclip: to copy path to clipboard
     """
 
     # Adjust the source path according to the detected OS
-    if platform.system() == "Darwin":
-        source_path = "~/Desktop/"
-        system_OS = "Mac"
-    elif platform.system() == "Linux":
-        source_path = "~/Pictures/"
-        system_OS = "Linux"
+    image_path, system_OS = system_image_path()
 
-    # Let's try a different approach to this. Note that we need some adjustment
-    # to the path to account for globs apparent inability to work with tilde
-    # characters
     list_of_files = sorted(
-        glob.glob(os.path.expanduser(source_path) + '*.png'),
+        glob.glob(os.path.expanduser(image_path) + '*.png'),
         key=os.path.getmtime)
 
-    # Find the most recent n files in the source directory.
-    subset_file_list = list_of_files[n_files * -1:]
+    # Find the most recent file in the source directory.
+    original_image = list_of_files[-1]
 
     # Create a file path for the new file. Note that here were are going to
     # assume there is an 'images' directory in the current location
-    new_path = os.getcwd() + "/images/"
+    file_destination_path = os.path.normpath(file_destination_path)
+    
+    new_path = file_destination_path + "/images/"
     if not os.path.exists(new_path):
         os.makedirs(new_path)
 
-    # Note that this is now a loop: starting to build in functionality to 
-    # accept multiple files
-    markdown_return = []
+    # Add an (optimistically) more informative title, and transform to replace
+    # spaces with underscores
+    new_file_name = input("File name: ")
+    new_file_name = datetime.now().strftime("%d-%m-%Y %H %M %S") + \
+        " " + new_file_name + ".png"
+    new_file_name = new_file_name.replace(" ", "_")
 
-    for file_name in subset_file_list:
+    # Move the file
+    new_file_path = new_path + new_file_name
 
-        # Add an (optimistically) more informative title, and transform to replace
-        # spaces with underscores
-        new_file_name = input("File name: ")
-        new_file_name = datetime.now().strftime("%d-%m-%Y %H %M %S") + " " + new_file_name + ".png"
-        new_file_name = new_file_name.replace(" ", "_")
+    # TODO: this is probably a better approach to this. Kill the other function.
+    # Note that this could be destructive. Going to change to copy.
+    copyfile(original_image, new_file_path)
+    print("Moved image to:", new_file_path)
 
-        new_file_path = os.getcwd() + "/images/" + new_file_name
+    # Now we need the path to the image 
+    relative_path = "./images/" + os.path.basename(new_file_path)
 
-        # Move the file and confirm to the user where this file has been moved
-        os.system("cp " + "'" + file_name + "' " + new_file_path)
-        print("Saved new file to: " + new_file_path)
+    # Now let's format this for markdown
+    markdown_path = "![](" + relative_path + ")"
 
-        # Note here that it's best to insert images with relative paths
-        abs_path = os.getcwd()
-        cut_it = abs_path.split("/")[-1]
-        relative_path = "./images/" + new_file_name
-
-        # Reformat this thing to markdown, and return to a new list
-        markdown_return.append("![](" + relative_path + ")")
-
-    # Save the new filepath to the clipboard
-    # TODO has this multiple markdown functionality been tested?
-    if system_OS == "Mac":
-        if len(markdown_return) == 1:
-            os.system("echo " + "'" + markdown_return[0] + "'" + " | pbcopy")
-        else:
-            several_markdown_values = "\n".join(str(e) for e in markdown_return)
-            os.system("printf " + "'" + several_markdown_values + "'" +
-                    " | pbcopy")
-    elif system_OS == 'Linux':
-        if len(markdown_return) == 1:
-            os.system("echo " + "'" + markdown_return[0] + "'" + " | xclip -sel clip")
-        else:
-            several_markdown_values = "\n".join(str(e) for e in markdown_return)
-            os.system("printf " + "'" + several_markdown_values + "'" +
-                    " | xclip -sel clip")
+    # Copy to the clipboard
+    string_to_clipboard(markdown_path, system_OS)
 
 
 def notebook_to_markdown(notebook_directory_name):
@@ -96,10 +103,10 @@ def notebook_to_markdown(notebook_directory_name):
 
     Args:
         notebook_directory_name ([type]): [description]
-    
+
     TODO 
     * This is starter code only. 
-    """    
+    """
 
     # Find all .ipynb suffix files and their FULL path
     notebook_paths = glob.glob("./**/*.ipynb", recursive=True)
@@ -115,9 +122,8 @@ def notebook_to_markdown(notebook_directory_name):
         new_path = "/".join(path_list)
 
         # Convert, and save the new file
-        #jupyter nbconvert --to html mynotebook.ipynb
-        #os.system()
-
+        # jupyter nbconvert --to html mynotebook.ipynb
+        # os.system()
 
         # I think we need some control flow to ensure that, under cases where the
         # directory structure is not present, it is created.
@@ -138,7 +144,7 @@ def find_images_in_markdown(markdown_file_path):
     * I could really make better use of these regex objects. For now, I'm 
     parsing things in a way that is... "not great". 
     """
-    #TODO review: want this to find local file paths, _not_ external links.
+    # TODO review: want this to find local file paths, _not_ external links.
     regex = re.compile("(?:!\[(.*?)\]\((.*?)\))")
 
     matches = []
@@ -147,20 +153,21 @@ def find_images_in_markdown(markdown_file_path):
         for line in f:
             result = regex.search(line)
             if result != None:
-                
+
                 # This is about to be some garbage code
-                # For posterity, there is a better way to access the results 
+                # For posterity, there is a better way to access the results
                 # of re matches -- as follows
                 a_match = result.group(0)
 
                 # All we really care about is the image stuff (between parentheses)
-                image_path = a_match[a_match.find("(")+1:a_match.find(")")].replace('"', "")
+                image_path = a_match[a_match.find(
+                    "(")+1:a_match.find(")")].replace('"', "")
                 matches.append(image_path)
 
-    # TODO what happens if there are no matches? What do we want to return 
-    # from this? None? 
+    # TODO what happens if there are no matches? What do we want to return
+    # from this? None?
     print("Found", len(matches), "image links")
-    
+
     return matches
 
 
@@ -173,7 +180,7 @@ def move_file(source_file_path, destination_dir_path):
         move
         destination_dir_path ([type]): the path to the directory where the file 
         should be moved. 
-    
+
     TODO 
     * This function could be used in a number of other locations, above. This 
     would require a refactor... That I don't really feel like doing right now.
@@ -201,10 +208,10 @@ def move_markdown(source_file_path, destination_dir_path):
     Let's assume there are some regular patterns here. 
     1. Images are always going to be located in the same directory as the 
     markdown file (titled `images`)
-    
+
     TODO I should write another function that confirms that the files have been
     moved
-    """    
+    """
 
     # Find images in the file
     image_paths = find_images_in_markdown(source_file_path)
@@ -215,19 +222,19 @@ def move_markdown(source_file_path, destination_dir_path):
         if destination_dir_path[-1] != "/":
             destination_dir_path = destination_dir_path + "/"
 
-        # Confirm that the new destination has an images directory 
+        # Confirm that the new destination has an images directory
         new_image_path = destination_dir_path + "images/"
         if not os.path.exists(new_image_path):
             os.makedirs(new_image_path)
 
         # Move image files files
         for one_image_path in image_paths:
-            
-            # TODO I don't love how verbose this is, but this is a much better 
-            # way to work with paths. Change this elsewhere... 
+
+            # TODO I don't love how verbose this is, but this is a much better
+            # way to work with paths. Change this elsewhere...
             one_image_path = os.path.dirname(
                 source_file_path) + "/" + one_image_path
-            
+
             move_file(one_image_path, new_image_path)
 
     # Move the original file
@@ -256,7 +263,7 @@ def image_dir_cleanup(base_directory):
     Let's make another function, adding a hidden directory for each file, to 
     contain all of the images embedded within... That was a dramatic way 
     to put that.
-    
+
     base_directory: the starting point for finding all of the files that 
     you want to create specific image directories for
     """
@@ -318,7 +325,7 @@ def rename_file_references(source_file_path, new_path):
     if os.path.exists(source_file_path) == True:
 
         print("Found file: ", source_file_path)
-        
+
         with open(source_file_path, 'r') as file:
             filedata = file.read()
 
